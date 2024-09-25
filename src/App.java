d1fd638de0eba7519d3eb253dbf4d90352d28340
Karina <wendy123kary@gmail.com>
2024-09-25 13:12:55 -0500

    Metos Ordenamientos

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Metodos metodosOrdenamiento = new Metodos();
+
+        int[] arreglo = {5, 7, 30, 12, 9};
+        int[] arregloOrdenado = metodosOrdenamiento.sortByBubble(arreglo);
+        metodosOrdenamiento.printArreglo(arregloOrdenado);
+        System.out.print("\n -- -- -- \n");
+
+        int[] arregloOrdenadoDesendente = metodosOrdenamiento.sortByBubbleDesendente(arreglo);
+        metodosOrdenamiento.printArreglo(arregloOrdenadoDesendente);
+        System.out.print("\n -- -- -- \n");
+
+        //Ordenar las palabras alfabeticamente de la cadena de texto:
+        String cadena= "este es un ejemplo de texto para Ordenar";
+        String [] palabras= cadena.split(" ");
+        String [] palabrasOrdenadas= metodosOrdenamiento.sortByBubble(palabras);
+        metodosOrdenamiento.printArreglo(palabrasOrdenadas);
     }
 }
